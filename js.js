d6d0b2288c8639746e0768b9e9cff4e9ca0cef10
jusznvm <jusznvanmeeteren@gmail.com>
2021-10-24 01:52:15 +0200

    hurr

--- js.js
+++ js.js
@@ -1,15 +1,44 @@
-document.addEventListener('play', function(e){  
-    var audios = document.getElementsByTagName('audio');  
-    for(var i = 0, len = audios.length; i < len;i++){  
-        if(audios[i] != e.target){  
-            audios[i].pause();  
-        }  
-    }  
-}, true);
+const trackList = ["piano1", "piano2", "rhodes1", "rhodes2", "synth1", "synth2", "synth3", "white_noise"]
+
+// document.addEventListener('play', function (e) {
+//     var audios = document.getElementsByTagName('audio');
+//     for (var i = 0, len = audios.length; i < len; i++) {
+//         if (audios[i] != e.target) {
+//             audios[i].pause();
+//         }
+//     }
+// }, true);
 
 
-function mutePage() {
-    var elems = document.querySelectorAll("video, audio");
-
-    [].forEach.call(elems, function(elem) { muteMe(elem); });
+function generateAudioPlayer(src, id) {
+    let audioObj = `      
+    <div id="audio` + id + `">
+      <audio
+        controls
+        src="` + src + `.mp3">
+        Your browser does not support the
+        <code>audio</code> element.
+      </audio>
+    </div>
+    `
+    return audioObj;
 }
+
+function generateTracks() {
+    let globalHTML = document.getElementsByClassName("global")[0];
+    for (let j = 0; j < 5; j++) {
+        globalHTML.innerHTML += `<div class="control ` + j + `">`
+        for (let k = 0; k < trackList.length; k++) {
+            globalHTML.innerHTML += generateAudioPlayer(trackList[k], k);
+        }
+        globalHTML.innerHTML +=
+            `
+            <div>
+            <br>
+            <br>
+            <br>
+        `
+    }
+}
+
+generateTracks();
