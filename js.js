5685e5e1f4527d1bd0d880d1af24ab4de17f1ac7
jusznvm <jusznvanmeeteren@gmail.com>
2021-10-24 02:08:16 +0200

    glhf

--- js.js
+++ js.js
@@ -1,15 +1,11 @@
 const trackList = ["piano1", "piano2", "rhodes1", "rhodes2", "synth1"]
 
-// document.addEventListener('play', function (e) {
-//     var audios = document.getElementsByTagName('audio');
-//     for (var i = 0, len = audios.length; i < len; i++) {
-//         if (audios[i] != e.target) {
-//             audios[i].pause();
-//         }
-//     }
-// }, true);
-
-
+/**
+ * Deze functie genereert een audio object.
+ * @param {*} src is de naam van de file
+ * @param {*} id wordt gebruikt om elke audio object van elkaar te scheiden
+ * @returns 
+ */
 function generateAudioPlayer(src, id) {
     let audioObj = `      
     <div id="audio` + id + `">
@@ -24,6 +20,12 @@ function generateAudioPlayer(src, id) {
     return audioObj;
 }
 
+/**
+ * Is nog niet af
+ * 
+ * Deze functie genereert de tracks, eerst 5 keer een track en in elke track
+ * 5 audio objecten (zoveel als in de lijst van tracks)
+ */
 function generateTracks() {
     let globalHTML = document.getElementsByClassName("global")[0];
     for (let j = 0; j < 5; j++) {
@@ -40,5 +42,17 @@ function generateTracks() {
         `
     }
 }
+
+
+// document.addEventListener('play', function (e) {
+//     var audios = document.getElementsByTagName('audio');
+//     for (var i = 0, len = audios.length; i < len; i++) {
+//         if (audios[i] != e.target) {
+//             audios[i].pause();
+//         }
+//     }
+// }, true);
+
+
 
 generateTracks();
